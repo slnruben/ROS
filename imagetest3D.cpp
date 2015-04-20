d4297a1a74291d2fe4342cb2fec7644a6d7acd03
Rubén Sánchez <ruben@ruben.(none)>
2015-04-20 17:55:28 +0200

    percepcion con reconocimiento de objetos y .h

--- imagetest3D.cpp
+++ imagetest3D.cpp
@@ -1,71 +1,74 @@
-class ImageConverter3D {
-	ros::NodeHandle nh_;
-	ros::Subscriber image_sub_;
-	ros::Publisher image_pub_;
+#include "imagetest3D.h"
 
-	int hupper, hlower;
-	int supper, slower;
-	int vupper, vlower;
-
-	float vmh, vms, vmv;
-	float c;
-
-public:
-	ImageConverter3D() {
+	Imagetest3D::Imagetest3D() {
 		//std::string topic = nh_.resolveName("point_cloud");
 		image_sub_ = nh_.subscribe<sensor_msgs::PointCloud2>(
 				"/camera/depth_registered/points", 1,
-				&ImageConverter3D::imageCb, this);
+				&Imagetest3D::Imagetest3D::imageCb, this);
 		image_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/pc_filtered", 1);
+		HUORANGE = 360; //ImageConverter3D::HURANGE
+		HLORANGE = 360; //336;
+		SUORANGE = 360; //360;
+		SLORANGE = 360; //276;
+	
+		HUBIGORANGE = 360; //35;
+		HLBIGORANGE = 360; //0;
+	
+		HURED = 360; //360;
+		HLRED = 360; //336;
+		SURED = 360; //275;
+		SLRED = 360; //180;
+	
+		HUBLUE = 360; //265;
+		HLBLUE = 360; //150;
+	
+		HUYELLOW = 360; //149;
+		HLYELLOW = 360; //36;
+		
+		HUPINK = 360; //335;
+		HLPINK = 360; //266;
+		distpix = 10;
+		sizemin = 20;
 
-		cvNamedWindow("Imagen Fuente");
+		maxrange = 1.0;
+		minrange = -1.0;
+
 		cvNamedWindow("Imagen filtrada");
+		cvNamedWindow("Filtrador Pelotas");
+		cvNamedWindow("Filtrador Balizas");
 
-		// TrackBar
-		cvCreateTrackbar("Hue Upper", "Imagen filtrada", &hupper, 360, NULL);
-		cvCreateTrackbar("Hue Lower", "Imagen filtrada", &hlower, 360, NULL);
-		cvCreateTrackbar("Sat Upper", "Imagen filtrada", &supper, 360, NULL);
-		cvCreateTrackbar("Sat Lower", "Imagen filtrada", &slower, 360, NULL);
-		cvCreateTrackbar("Val Upper", "Imagen filtrada", &vupper, 360, NULL);
-		cvCreateTrackbar("Val Lower", "Imagen filtrada", &vlower, 360, NULL);
-		//cvCreateButton("Save",ImageConverter::callbackButton,NULL,CV_PUSH_BUTTON,0);
-		//Pelota roja clara pequeña
-		//hupper = 360;
-		//hlower = 319;
-		//vupper = 360;
-		//vlower = 300;
-		//supper = 360;
-		//slower = 313;
-		//Pelota roja oscura pequeña
-		//hupper = 350;
-		//hlower = 330;
-		//vupper = 359;
-		//vlower = 220;
-		//supper = 260;
-		//slower = 120;
-		//Pelota naranja grande
-		//hupper = 360;
-		//hlower = 340;
-		//vupper = 180;
-		//vlower = 120;
-		//supper = 360;
-		//slower = 350;
-		//Baliza azul, rosa, blanca
-		//hupper = 310;
-		//hlower = 210;
-		//vupper = 320;
-		//vlower = 50;
-		//supper = 360;
-		//slower = 180;
+		cvCreateTrackbar("Hue Upper ORANGE", "Filtrador Pelotas", &HUORANGE, 360, NULL);
+		cvCreateTrackbar("Hue Lower ORANGE", "Filtrador Pelotas", &HLORANGE, 360, NULL);
+		cvCreateTrackbar("Sat Upper ORANGE", "Filtrador Pelotas", &SUORANGE, 360, NULL);
+		cvCreateTrackbar("Sat Lower ORANGE", "Filtrador Pelotas", &SLORANGE, 360, NULL);
+
+		cvCreateTrackbar("Hue Upper RED", "Filtrador Pelotas", &HURED, 360, NULL);
+		cvCreateTrackbar("Hue Lower RED", "Filtrador Pelotas", &HLRED, 360, NULL);
+		cvCreateTrackbar("Sat Upper RED", "Filtrador Pelotas", &SURED, 360, NULL);
+		cvCreateTrackbar("Sat Lower RED", "Filtrador Pelotas", &SLRED, 360, NULL);
+
+		cvCreateTrackbar("Hue Upper BIGORANGE", "Filtrador Pelotas", &HUBIGORANGE, 360, NULL);
+		cvCreateTrackbar("Hue Lower BIGORANGE", "Filtrador Pelotas", &HLBIGORANGE, 360, NULL);
+
+		cvCreateTrackbar("Hue Upper BLUE", "Filtrador Balizas", &HUBLUE, 360, NULL);
+		cvCreateTrackbar("Hue Lower BLUE", "Filtrador Balizas", &HLBLUE, 360, NULL);
+
+		cvCreateTrackbar("Hue Upper PINK", "Filtrador Balizas", &HUPINK, 360, NULL);
+		cvCreateTrackbar("Hue Lower PINK", "Filtrador Balizas", &HLPINK, 360, NULL);
+
+		cvCreateTrackbar("Hue Upper YELLOW", "Filtrador Balizas", &HUYELLOW, 360, NULL);
+		cvCreateTrackbar("Hue Lower YELLOW", "Filtrador Balizas", &HLYELLOW, 360, NULL);
+
+		cvCreateTrackbar("Distacia pixeles", "Filtrador Balizas", &distpix, 100, NULL);
+		cvCreateTrackbar("Tamaño minimo", "Filtrador Balizas", &sizemin, 100, NULL);
 
 	}
 
-	~ImageConverter3D() {
-		cv::destroyWindow("Imagen Fuente");
+	Imagetest3D::~Imagetest3D() {
 		cv::destroyWindow("Imagen filtrada");
 	}
 
-	void imageCb(const sensor_msgs::PointCloud2ConstPtr& msg) {
+	void Imagetest3D::imageCb(const sensor_msgs::PointCloud2ConstPtr& msg) {
 		pcl::PointCloud<pcl::PointXYZRGB> PCxyzrgb, PCxyzrgbout;
 		sensor_msgs::PointCloud2 out;
 
@@ -77,36 +80,59 @@ public:
 		//The points of this resulting point cloud will be added later, depending on the H filtering process.
 		PCxyzrgbout = PCxyzrgb;
 		PCxyzrgbout.clear();
-
-		vmh = 0.0;
-		vms = 0.0;
-		vmv = 0.0;
-		c = 0.0;
-
+		initObjetos();
 		pcl::PointCloud<pcl::PointXYZRGB>::iterator it;
 		for (it = PCxyzrgb.begin(); it != PCxyzrgb.end(); ++it) {
 			pcl::PointXYZHSV hsv;
 			pcl::PointXYZRGBtoXYZHSV(*it, hsv);
-
+			//ROS_INFO("%f %f %f", it->x, it->y, it->z);
+			
 			//if the point is in the H range, it is added to the resulting point cloud. On the other hand, paint it black (to be displayed later)
-			if ((hsv.h >= hlower) && (hsv.h <= hupper) && (hsv.s >= (float)slower/360) && (hsv.s <= (float)supper/360) && 
-																(hsv.v >= (float)vlower/360) && (hsv.v <= (float)vupper/360)) { 
-
-				PCxyzrgbout.push_back(*it);
-				vmh += hsv.h;
-				vms += hsv.s * 360.0;
-				vmv += hsv.v * 360.0;
-				c++;
-			} else {
+			if(it->x == it->x){
+				if (((hsv.h >= HLORANGE) && (hsv.h <= HUORANGE)) && ((hsv.s >= ((float)SLORANGE/360)) && (hsv.s <= ((float)SUORANGE/360)))){
+					//ROS_INFO("Filtra Naranja");
+					PCxyzrgbout.push_back(*it);
+					addNode(ORANGE, it->x, it->y, it->z);
+				}else if(((hsv.h >= HLRED) && (hsv.h <= HURED)) && ((hsv.s >= ((float)SLRED/360)) && (hsv.s <= ((float)SURED/360)))){
+					//ROS_INFO("Filtra Rojo");
+					PCxyzrgbout.push_back(*it);
+					addNode(RED, it->x, it->y, it->z);				
+				}else if(((hsv.h >= HLBIGORANGE) && (hsv.h <= BIGORANGE))){
+					//ROS_INFO("Filtra Naranja claro");
+					PCxyzrgbout.push_back(*it);
+					addNode(BIGORANGE, it->x, it->y, it->z);
+				}else if(((hsv.h >= HLBLUE) && (hsv.h <= HUBLUE))){
+					//ROS_INFO("Filtra Azul");
+					PCxyzrgbout.push_back(*it);
+					addNode(BLUE, it->x, it->y, it->z);
+				}else if(((hsv.h >= HLYELLOW) && (hsv.h <= HUYELLOW))){
+					//	ROS_INFO("Filtra amarillo");
+					PCxyzrgbout.push_back(*it);
+					addNode(YELLOW, it->x, it->y, it->z);
+				}else if(((hsv.h >= HLPINK) && (hsv.h <= HUPINK))){
+					//ROS_INFO("Filtra rosa");
+					PCxyzrgbout.push_back(*it);
+					addNode(PINK, it->x, it->y, it->z);
+				}else{
+					//ROS_INFO("No filtrado***********************************************************************");
+					it->r = 0;
+					it->g = 0;
+					it->b = 0;
+				}
+			}else{
 				it->r = 0;
 				it->g = 0;
 				it->b = 0;
 			}
 		}
+		//Filtrado de objetos
+		//for(int i = 0; i < NUM_COLORS; i++){
+		filtrarObjetos();
+		//}
 
-		ROS_INFO("Valor h: %f", vmh/c);
-		ROS_INFO("Valor s: %f", vms/c);
-		ROS_INFO("Valor v: %f", vmv/c);
+		//Percepcion de Objetos
+		
+			//Pendiente
 
 		//tansform PCxyzrgb (pcl::PointCloud<pcl::PointXYZRGB>) to Image to display in the OpenCV window
 		pcl::toROSMsg(PCxyzrgb, out);
@@ -117,45 +143,206 @@ public:
 		cv_imageout = cv_bridge::toCvCopy(image,
 				sensor_msgs::image_encodings::BGR8);
 
-		cv::imshow("Imagen fuente", cv_imageout->image);
-		cv::waitKey(3);
-
-		//Get the mean position of the resulting point cloud
-
-		float x, y, z;
-		x = y = z = 0.0;
-		int c = 0;
-
-		for (it = PCxyzrgbout.begin(); it != PCxyzrgbout.end(); ++it) {
-
-			if (it->x == it->x) //This seems to be the only way to detect if it is NaN
-					{
-				x = x + it->x;
-				y = y + it->y;
-				z = z + it->z;
-				c++;
+		for (int i = 0; i < NUM_COLORS; i++){
+			NodeColor *auxnode = objetos[i].list;
+			while(auxnode != NULL){
+				auxnode->cy = (auxnode->cy / 0.00625) + 240;
+				auxnode->cx = (auxnode->cx / 0.0055) + 320;
+				ROS_INFO("COLOR: %d  CX: %f  CY: %f  CZ: %f SIZE: %f",i, auxnode->cx, auxnode->cy, auxnode->cz, auxnode->total);
+				cv::rectangle(cv_imageout->image, cv::Point(auxnode->cx-10, auxnode->cy-10), cv::Point(auxnode->cx+10, auxnode->cy+10), cv::Scalar(0, 0, 255), 1, 8);
+				auxnode = auxnode->next;
 			}
 		}
 
-		if (c > 0) {
-			x = x / (float) c;
-			y = y / (float) c;
-			z = z / (float) c;
-		}
-		std::cout << "Center of the point cloud filtered = (" << x << ", " << y
-				<< ", " << z << ") [" << c << "]" << std::endl;
+		cv::imshow("Imagen filtrada", cv_imageout->image);
+		cv::waitKey(3);
+
+		//std::cout << "Center of the point cloud filtered = (" << x << ", " << y
+		//		<< ", " << z << ") [" << c << "]" << std::endl;
 
 		//tranform PCxyzrgbout (pcl::PointCloud<pcl::PointXYZRGB>) to PointCloud2 in order to be published
 		sensor_msgs::PointCloud2 pcout;
 		pcl::toROSMsg(PCxyzrgbout, pcout);
 		image_pub_.publish(pcout);
-
+		freeList();
 	}
-};
+
+	void Imagetest3D::initColores() {
+		for(int i = 0; i < NUM_COLORS; i++){
+			objetos[i].list = NULL;
+		}
+	}
+
+	Imagetest3D::NodeColor* Imagetest3D::newNodeColor(float x, float y, float z) {
+		NodeColor *node = NULL;
+		if((node = new NodeColor) != NULL){
+			node->cx = x;
+			node->cy = y;
+			node->cz = z;
+			node->total = 1.0;
+			node->next = NULL;
+			node->prev = NULL;
+		}
+		return node;
+	} 
+	
+	void Imagetest3D::addNode(int color, float x, float y, float z) {
+		if((objetos[color].list == NULL)){
+			objetos[color].list = newNodeColor(x, y, z);
+		}else{
+
+			NodeColor *node = objetos[color].list;
+			while(node != NULL){
+				if(compPixel(node, x, y, z)){
+					float xaux, yaux, zaux;
+					xaux = yaux = zaux = 0.0;
+					xaux = (node->cx * node->total) + x;
+					yaux = (node->cy * node->total) + y;
+					zaux = (node->cz * node->total) + z;
+					node->total = node->total + 1.0;
+					node->cx = xaux / node->total;
+					node->cy = yaux / node->total;
+					node->cz = zaux / node->total;
+					//ROS_INFO("x: %f - %f, Y: %f -%f, z: %f - %f, size: %f", x,node->cx, y,node->cy, z, node->cz, node->total);
+					break;
+				}
+				node = node->next;
+			}
+			if(node == NULL){
+				node = newNodeColor(x, y, z);
+				objetos[color].list->prev = node;
+				node->next = objetos[color].list;
+				objetos[color].list = node;
+			}
+		}
+	}
+
+	int Imagetest3D::compPixel(NodeColor *node, float x, float y, float z) {
+		float px = (x - node->cx);
+		float py = (y - node->cy);
+		float pz = (z - node->cz);
+		float total = sqrt(px * px + py * py + pz * pz);
+		if(total < (float)distpix/100.0){
+			return 1;
+		}
+		return 0;
+	}
+
+	void Imagetest3D::filtrarObjetos() {
+		
+		for(int i = 0; i < NUM_COLORS; i++){
+			NodeColor *node = objetos[i].list;
+			NodeColor *aux = NULL;
+			while(node != NULL){
+				if(node->total < (float)sizemin){
+					if(node == objetos[i].list)
+						objetos[i].list = node->next;
+					aux = node->prev;
+					node = removeNode(node);
+					if(aux != NULL)
+						aux->next = node;
+					if(node != NULL)
+						node->prev = aux;
+					 
+				}else{
+					//ROS_INFO("X: %d,  Y: %d \n", node->cx, node->cy);
+					node = node->next;			
+				}
+			}
+		}
+	}
+
+	Imagetest3D::NodeColor* Imagetest3D::removeNode(NodeColor* node) {
+		NodeColor *aux = node->next;
+		delete node;
+		return aux;
+	}
+
+
+	void Imagetest3D::freeList() {
+		NodeColor *node = NULL;
+		for(int i = 0; i < NUM_COLORS; i++){
+			node = objetos[i].list;
+			while(node != NULL){
+				node = removeNode(node);
+			}
+			objetos[i].list = node;
+		}
+	}
+
+	void Imagetest3D::initObjetos() {
+		for(int i = 0; i <= NUM_OBJECTS; i++) {
+			//array[i].name = std::to_string(i);
+			array[i].boolean = 0;
+		}
+	}
+
+	void Imagetest3D::addArray(NodeColor *node, int i) {
+		array[i].cx = node->cx;
+		array[i].cy = node->cy;
+		array[i].cz = node->cz;
+		array[i].boolean = 1;
+	}
+
+	void Imagetest3D::reconnaissance() {
+		searchBaliza(objetos[PINK].list, objetos[BLUE].list, 0);
+		searchBaliza(objetos[YELLOW].list, objetos[BLUE].list, 1);
+		searchBaliza(objetos[BLUE].list, objetos[PINK].list, 2);
+		searchBaliza(objetos[YELLOW].list, objetos[PINK].list, 3);
+		searchPorteria(objetos[YELLOW].list, 4);
+		searchPorteria(objetos[BLUE].list, 5);
+	}
+
+	void Imagetest3D::searchBaliza(NodeColor *top, NodeColor *bot, int i) {
+		NodeColor *nodeTop = top;
+		NodeColor *nodeBot = bot;
+		int found = 0;
+		float errormarginY;
+		float errormarginZ;
+
+		while(nodeTop != NULL) {
+			while(nodeBot != NULL) {
+				errormarginY = nodeTop->cy - nodeBot->cy;
+				errormarginZ = nodeTop->cz - nodeBot->cz;
+				if(errormarginY <= maxrange &&  errormarginY >= minrange &&
+					errormarginZ <= maxrange && errormarginZ >= minrange) {
+					found = 1;
+					addArray(nodeTop, i);
+					break;
+				}else{
+					nodeBot = nodeBot->next;
+				}
+			}
+			if(found) {
+				//removeNode(nodeTop);
+				//removeNode(nodeBot);
+				break;
+			}else{
+				nodeTop = nodeTop->next;
+			}
+		}
+	}
+
+	void Imagetest3D::searchPorteria(NodeColor *node, int pos) {
+		int max;
+		for(int i = 1; i < 3; i++) {
+		 	NodeColor *aux = NULL;
+			max = 0;
+			while(node != NULL){
+				if(node->total > max) {
+					aux = node;
+					max = node->total;
+				}
+				node = node->next;
+			}
+			addArray(aux, pos);
+		}
+	}
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "image_converter3D");
-	ImageConverter3D ic;
+	Imagetest3D ic;
 	ros::spin();
 	return 0;
 }
+
--- imagetest3D.h
+++ imagetest3D.h
@@ -1,3 +1,6 @@
+#ifndef imagetest3D_H
+#define imagetest3D_H
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
@@ -11,27 +14,89 @@
 #include <pcl/point_types.h>
 #include <pcl/impl/point_types.hpp>
 #include <pcl/point_types_conversion.h>
+#include <string>
 
 class Imagetest3D {
 private:
-	ros::NodeHandle nh_;
-	ros::Subscriber image_sub_;
-	ros::Publisher image_pub_;
+	enum{ORANGE, BIGORANGE, RED, BLUE, YELLOW, PINK, NUM_COLORS};
 
-	int hupper, hlower;
-	int supper, slower;
-	int vupper, vlower;
-
-	float vmh, vms, vmv;
-	float c;
-	typedef struct ColorNode ColorNode;
-	struct ColorNode {
+	//typedef struct NodeColor NodeColor;
+	//typedef struct ListObjetos ListObjetos;
+	struct NodeColor{
 		float cx;
 		float cy;
 		float cz;
-		PointCloud2 p;
-		int n;
-		ColorNode next;
+		float total;
+		//pcl::PointCloud<pcl::PointXYZRGB> cloud;
+		NodeColor *next;
+		NodeColor *prev;
 	};
-	enum {NARANJAG, NARANJAP, ROJA, ROSA, AZUL, AMARILLO}
-}
+
+	struct ListColores{
+		NodeColor *list;
+	};
+
+	struct Objects{
+		std::string name;
+		float cx;
+		float cy;
+		float cz;
+		int boolean;
+	};
+
+	enum{BALIZA1, BALIZA2, BALIZA3, BALIZA4, PORTERIA1, PORTERIA2, PELOTA1, PELOTA2, PELOTA3, NUM_OBJECTS};
+
+	Objects array [NUM_OBJECTS];
+
+	ros::NodeHandle nh_;
+	ros::Subscriber image_sub_;
+	ros::Publisher image_pub_;
+	
+	ListColores objetos[NUM_COLORS];
+
+	int HUORANGE; //ImageConverter3D::HURANGE
+	int HLORANGE;
+	int SUORANGE;
+	int SLORANGE;
+
+	int HUBIGORANGE;
+	int HLBIGORANGE;
+
+	int HURED;
+	int HLRED;
+	int SURED;
+	int SLRED;
+
+	int HUBLUE; //265;
+	int HLBLUE; //150;
+
+	int HUYELLOW;
+	int HLYELLOW;
+	
+	int HUPINK;
+	int HLPINK;
+
+	int distpix;
+	int sizemin;
+
+	float minrange;
+	float maxrange;
+public:
+	Imagetest3D();
+	~Imagetest3D();
+	void imageCb(const sensor_msgs::PointCloud2ConstPtr& msg);
+	void initColores();
+	NodeColor* newNodeColor(float x, float y, float z);
+	void addNode(int color, float x, float y, float z);
+	int compPixel(NodeColor *node, float x, float y, float z);
+	void filtrarObjetos();
+	NodeColor* removeNode(NodeColor* node);
+	void freeList();
+	void initObjetos();
+	void addArray(NodeColor *node, int i);
+	void reconnaissance();
+	void searchBaliza(NodeColor *top, NodeColor *bot, int i);
+	void searchPorteria(NodeColor *node, int pos);
+};
+
+#endif
