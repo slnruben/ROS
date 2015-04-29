a86e4332de5a378c45857a9da81922973e8281ed
mariano <mariano@mariano-MacBookAir.(none)>
2015-04-29 10:12:09 +0200

    localizacion casi

--- Localizacion/pruebaLocalizacion.cpp
+++ Localizacion/pruebaLocalizacion.cpp
@@ -13,7 +13,8 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, std::string("localization"));
 	ros::NodeHandle n;
 
-	
+	tf::StampedTransform W2BL;
+	tf::TransformBroadcaster tfB;
 	ros::Publisher pose_pub;
 	MCL mcl;
 	geometry_msgs::PoseWithCovarianceStamped poserobot;
@@ -22,27 +23,46 @@ int main(int argc, char **argv)
 
 	ros::Rate loop_rate(10);
 	int count = 0;
-	sleep(3);
+	sleep(5);
 	while (ros::ok())
 	{
 	
 		//===============================================================================
 		//Publish Pose
-		mcl.correct();
+		//mcl.predict();
 		poserobot = mcl.getPose();
-		//mcl.publishInfo();
 		
-		/*std::cout<<"Real pose: "<<poserobot.pose.pose.position.x<<", "<<
+		
+		
+		std::cout<<"Real pose: "<<poserobot.pose.pose.position.x<<", "<<
 				poserobot.pose.pose.position.y<<", "<<
-				poserobot.pose.pose.position.z<<")"<<std::endl;*/
+				poserobot.pose.pose.position.z<<")"<<std::endl;
 
-		poserobot.header.frame_id = "world";
+		poserobot.header.frame_id = "/world";
 		poserobot.header.stamp = ros::Time::now();
 		poserobot.header.seq = count;
 
+		W2BL.frame_id_ = "/world";
+		W2BL.child_frame_id_ = "/base_link";
+		W2BL.stamp_ = ros::Time::now() + ros::Duration(1.0);
+
+		W2BL.setOrigin(tf::Vector3(poserobot.pose.pose.position.x, poserobot.pose.pose.position.y, 0.0));
+		W2BL.setRotation(tf::Quaternion(poserobot.pose.pose.orientation.x, poserobot.pose.pose.orientation.y,
+			poserobot.pose.pose.orientation.z, poserobot.pose.pose.orientation.w));
+		
+		try{
+			tfB.sendTransform(W2BL);
+		}catch(tf::TransformException & ex){
+			ROS_WARN("%s",ex.what());
+		}
+
+		
 
 		pose_pub.publish(poserobot);
 		
+		mcl.correct();
+		mcl.publishInfo();
+
 		ros::spinOnce();
 
 		loop_rate.sleep();
